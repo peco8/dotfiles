d8cc0af68bafad2a9cd4874653813aff12978ffe
Toshiki <09c0507@gmail.com>
2015-12-30 16:32:33 +0900

    Add the start-up script to make it easier to get it started

---
+++ start_up.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+mkdir ~/.vim/tmp
+
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
