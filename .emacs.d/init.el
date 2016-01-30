c40175cee93de4e4ae67346e22be6aa074690750
Toshiki <09c0507@gmail.com>
2016-01-30 15:09:19 +0900

    Add emacs configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,32 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; el-get : Manage the external elisp bits and pieces upon which you depend! http://tapoueh.org/emacs/el-get.html
+;;; https://github.com/dimitri/el-get
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Basic setup
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+    (with-current-buffer
+            (url-retrieve-synchronously
+	             "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+	        (goto-char (point-max))
+		(eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+;; List all package name below you need to install, then el-get automatically install them
+
+; auto-complete
+(el-get-bundle auto-complete)
+
+; org
+(el-get-bundle org)
+
+; tuareg
+(el-get-bundle tuareg)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-hook 'after-init-hook '(lambda ()
+			      (load "~/.emacs.d/my-noexternals.el")
+			      ))
---
+++ .emacs.d/my-noexternals.el
@@ -0,0 +1,92 @@
+;;;;;;;;;;;;;;;;;;
+;;; Character code
+;;;;;;;;;;;;;;;;;;
+
+;; UTF-8 (set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;;;;;;;;;;;;;;;;;
+;;; Behavior
+;;;;;;;;;;;;;;;;;;
+
+;; Disable startup message
+(setq inhibit-startup-message t)
+
+;; Disable to create backup file
+(setq make-backup-files nil)
+
+;; Diable to create auto-save files
+(setq delete-auto-save-files t)
+
+;; Enlight the opposite cursor
+(show-paren-mode 1)
+
+;; Scroll by each line
+(setq scroll-conservatively 1)
+
+;; Modify the " yes or no " => " y or n "
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Disable beep and flash
+(setq ring-bell-function 'ignore)
+
+;; Enlight Trailing white space
+
+;; Chagne option to meta
+(setq mac-option-modifier 'meta)
+
+;; Deleting traring space and blank lines
+(add-hook 'before-save-hook 'delete-trailing-whitespace 'delete-blank-lines)
+
+;;;;;;;;;;;;;;;;;;
+;;; Design
+;;;;;;;;;;;;;;;;;;
+
+;; Disable pop up frames
+(setq ns-pop-up-frames nil)
+
+;; Disable menu bar
+(menu-bar-mode -1)
+
+;; Turn off toolbar
+(if window-system
+        (tool-bar-mode 0))
+
+;; Theme
+(load-theme 'manoj-dark t)
+
+;; Change the mini buffer color
+(face-spec-set 'minibuffer-prompt
+               '((((class color) (background light))
+                  (:foreground "cyan"))
+                 (t (:foreground "white"
+                     :background "brown"))))
+;; Mode line shows time
+(display-time-mode 1)
+(setq display-time-string-forms
+      '((propertize (concat " " 24-hours ":" minutes " ")
+ 		   'face 'egoge-display-time)))
+
+;; Mode line color for active
+(set-face-foreground 'mode-line "white")
+(set-face-background 'mode-line "Red")
+
+;; Mode line color for inactive
+(set-face-foreground 'mode-line-inactive "gray30")
+(set-face-background 'mode-line-inactive "gray85")
+
+;;;;;;;;;;;;;;;;;;
+;;; Keybindings
+;;;;;;;;;;;;;;;;;;
+
+;; Wind-move
+(global-set-key (kbd "C-c C-j") 'windmove-left)
+(global-set-key (kbd "C-c C-k") 'windmove-down)
+(global-set-key (kbd "C-c C-l") 'windmove-up)
+(global-set-key (kbd "C-c C-;") 'windmove-right)
